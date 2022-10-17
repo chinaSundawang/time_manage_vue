15b5b79af15c52a5a2d097ba589e80ff89600daa
sundawang <sundawang66@gmail.com>
2022-10-17 20:16:49 +0800

    引入el

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'; // 2.1引入结构
+import 'element-ui/lib/theme-chalk/index.css'; // 2.2引入样式
 
 import './assets/main.css'
 
 const app = createApp(App)
+Vue.use(ElementUI); // 3.安装
 
 app.use(router)
 
 app.mount('#app')
+
+new Vue({
+    el:'#app',
+    render: h=>(App)
+});
